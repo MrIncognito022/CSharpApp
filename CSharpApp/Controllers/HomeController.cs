1a9b87c02feecedd654b1e71f4ecf60da8a86e46
MrIncognito022 <101396617+MrIncognito022@users.noreply.github.com>
2024-06-09 04:01:32 +0500

    Add project files.

---
+++ CSharpApp/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSharpApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RunFunction()
+        {
+            return Content("This is the message from C# File");
+        }
+    }
+}
